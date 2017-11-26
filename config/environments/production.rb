30fe68a2609b2b81dd6cece87df580fc258dbe32
長谷部亮 <wtc.r.h173@gmail.com>
2017-11-26 19:06:30 +0900

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'fierce-tundra-71087.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '52.68.56.63' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
